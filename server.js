5703ee49d10244fd9c25cf4eb7dbb4800e2e0fed
Naveen Fernando <malinda0610@gmail.com>
2021-05-28 07:56:13 +0530

    first commit

--- server.js
+++ server.js
@@ -5,7 +5,14 @@ var cors = require('cors')
 
 const app = express();
 
-app.use(cors())
+app.use(cors({origin: true, credentials: true, methods: 'POST,GET,PUT,OPTIONS,DELETE'}))
+
+ app.use((req, res, next) => {
+   res.header("Access-Control-Allow-Origin", "*");
+   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+   next();
+})
 
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/angular-frontend-server'));
